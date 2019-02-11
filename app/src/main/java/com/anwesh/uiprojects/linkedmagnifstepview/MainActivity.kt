d1ef1ba50ce4ce1394c2db887d76bf9b21e5ce59
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 02:59:07 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmagnifstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmagnifstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedmagnifstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.magnifstepview.MagnifStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MagnifStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
