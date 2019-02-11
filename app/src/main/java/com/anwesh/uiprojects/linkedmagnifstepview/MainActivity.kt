07743efcc4eca031ac31840a7c6a52875e079d7b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 02:58:25 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedmagnifstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmagnifstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedmagnifstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.magnifstepview.MagnifStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MagnifStepView.create(this)
     }
 }
