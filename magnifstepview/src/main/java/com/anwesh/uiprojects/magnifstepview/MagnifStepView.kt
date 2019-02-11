6a2e30b44a13efbf97c65e420b5091a7190dfd2a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 02:45:40 +0530

    adding MSNode class

--- magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
+++ magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
@@ -120,4 +120,48 @@ class MagnifStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MSNode(var i : Int, val state : State = State()) {
+
+        private var next : MSNode? = null
+        private var prev : MSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MSNode {
+            var curr : MSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
