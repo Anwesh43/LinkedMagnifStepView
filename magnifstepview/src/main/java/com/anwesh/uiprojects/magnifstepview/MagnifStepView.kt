bb995eb163b910a95efef7a1365a8ebbf0658395
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 02:34:26 +0530

    adding drawMSNode canvas extension method

--- magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
+++ magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
@@ -21,10 +21,37 @@ val sizeFactor : Float = 2.9f
 val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#4527A0")
 val backColor : Int = Color.parseColor("#212121")
-
+val rotDeg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sf() : Float = 1f - 2 * this
+
+fun Canvas.drawMSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val y : Float = -2 * size / 3
+    val r : Float = (size -  Math.abs(y)) / 2
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc2.divideScale(j, lines)
+        save()
+        translate(w / 2, gap * (i + 1))
+        rotate(rotDeg * sc * j.sf())
+        drawLine(0f, 0f, 0f, y, paint)
+        save()
+        translate(0f, y)
+        drawArc(RectF(-r, -r, r, r), -90f, 360f * sc1, false, paint)
+        restore()
+        restore()
+    }
+}
