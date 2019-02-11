037e47433340f1ac0457feaa4098dcfa7deaac66
Anwesh43 <anweshthecool0@gmail.com>
2019-02-12 01:59:23 +0530

    adding libraries and adding constants

--- magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
+++ magnifstepview/src/main/java/com/anwesh/uiprojects/magnifstepview/MagnifStepView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.magnifstepview
 /**
  * Created by anweshmishra on 12/02/19.
  */
+
+import android.view.MotionEvent
+import android.view.View
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#212121")
